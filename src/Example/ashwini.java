c8deb9ffc70ab61484bac387b51f6b0c32064c2a
lenovo <lenovo@DESKTOP-TAPMII9>
2022-08-29 22:30:05 +0530

    [Ashwini] first changes in project

---
+++ src/Example/ashwini.java
@@ -0,0 +1,15 @@
+package Example;
+
+public class ashwini {
+
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi");
+		System.out.println("Ashwini");
+		System.out.println("abcd");
+		
+	}
+
+}
